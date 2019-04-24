843735b0e2697ecf8bb09a6dd6770bc32193e2fe
rishabh-j <rishabh_jain@iitg.ernet.in>
2019-04-24 08:55:49 +0530

    Add files via upload

--- ipl.cpp
+++ ipl.cpp
@@ -44,7 +44,7 @@ int main()
     vector<double> sure_top2(8,0);
     vector<double> maybe_top2(8,0);
 
-    int done      = 30;
+    int done      = 41;
     int total     = 56;
     int remaining = total-done;
 
@@ -52,17 +52,6 @@ int main()
     string s;
 
     match results[total+1];
-    results[31].home = MI;
-    results[32].home = KXIP;
-    results[33].home = SRH;
-    results[34].home = DC;
-    results[35].home = KKR;
-    results[36].home = RR;
-    results[37].home = DC;
-    results[38].home = SRH;
-    results[39].home = RCB;
-    results[40].home = RR;
-    results[41].home = CSK;
     results[42].home = RCB;
     results[43].home = KKR;
     results[44].home = CSK;
@@ -79,18 +68,6 @@ int main()
     results[55].home = KXIP;
     results[56].home = MI;
 
-
-    results[31].away = RCB;
-    results[32].away = RR;
-    results[33].away = CSK;
-    results[34].away = MI;
-    results[35].away = RCB;
-    results[36].away = MI;
-    results[37].away = KXIP;
-    results[38].away = KKR;
-    results[39].away = CSK;
-    results[40].away = DC;
-    results[41].away = SRH;
     results[42].away = KXIP;
     results[43].away = RR;
     results[44].away = MI;
@@ -106,26 +83,21 @@ int main()
     results[54].away = SRH;
     results[55].away = CSK;
     results[56].away = KKR;
-
-    int step = x/1000;
     vector<int> points;
 
     for(int t=0;t<x;t++)
     {
     	points.clear();
-        points.push_back(14);   //CSK
-        points.push_back(10);   //DC
+        points.push_back(16);   //CSK
+        points.push_back(14);   //DC
         points.push_back(8);    //KKR
-        points.push_back(8);    //KXIP
-        points.push_back(8);    //MI
-        points.push_back(2);    //RCB
-        points.push_back(4);    //RR
-        points.push_back(6);    //SRH
+        points.push_back(10);   //KXIP
+        points.push_back(12);   //MI
+        points.push_back(6);    //RCB
+        points.push_back(6);    //RR
+        points.push_back(10);    //SRH
 
-        if(t%step==0)
-            printf("Step : %d\n", t/step);
-
-        s = std::bitset<26>(t).to_string();
+        s = std::bitset<15>(t).to_string();
 
         for(int i=0;i<remaining;i++)
         {
@@ -191,20 +163,8 @@ int main()
         maybe_top4[i] *= 100;
     }
 
-    //Sort by sure_top4 (using bubble sort since there are only 8 elements and it is done only once)
     for(int i=0;i<8;i++)
-        for(int j=i+1;j<8;j++)
-            if(sure_top4[j]>sure_top4[i])
-            {
-                swap(teams[i],teams[j]);
-                swap(sure_top4[i],sure_top4[j]);
-                swap(maybe_top4[i],maybe_top4[j]);
-                swap(sure_top2[i],sure_top2[j]);
-                swap(maybe_top2[i],maybe_top2[j]);
-            }
-
-    for(int i=0;i<8;i++)
-        printf("%s \t SURE_TOP4 : %2.2f \t MAYBE_TOP4 : %2.2f \t SURE_TOP2 : %2.2f \t MAYBE_TOP2 : %2.2f\n",teams[i].c_str(),pakka_top4[i],maybe_top4[i],pakka_top2[i],maybe_top2[i]);
+        printf("%s \t SURE_TOP4 : %2.2f \t MAYBE_TOP4 : %2.2f \t SURE_TOP2 : %2.2f \t MAYBE_TOP2 : %2.2f\n",teams[i].c_str(),sure_top4[i],maybe_top4[i],sure_top2[i],maybe_top2[i]);
 
     return 0;
 }
